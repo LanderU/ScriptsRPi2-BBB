1d4cf68769b2dcceb3a5d0be6ec79bc45654dd4f
LanderU <lander.usategui@gmail.com>
2016-02-14 18:09:54 +0100

    Change crearimagen

--- Scripts/crearimagen.sh
+++ Scripts/crearimagen.sh
@@ -1,2 +1,6 @@
+#!/bin/bash
+
+read -p "Name of the new image? " name
+path="/Users/lander/ImagesFlash"
 sudo diskutil umount /dev/disk2s1 
-sudo dd if=/dev/rdisk2 bs=8m | gzip -c > erle-brain1-Debian-14-12-2015.img.gz
+sudo dd if=/dev/rdisk2 bs=8m | gzip -c > $path/$name.img.gz
