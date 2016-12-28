9ff68924beaa80f60e892b47f34f86f97e275f1e
LanderU <lander.usategui@gmail.com>
2016-12-28 20:33:18 +0100

    flashearV2.sh: Update, add extension test

--- flashearV2.sh
+++ flashearV2.sh
@@ -6,22 +6,38 @@
 
 '
 
+#####################
+####    COLORS   ####
+#####################
+
+redColor(){
+	tput setaf 1
+} # End red color
+
+resetColor(){
+	tput sgr 0
+} # End Reset color
+
+####################
+######  USER  ######
+####################
+
 currentUser=`whoami`
 
 clear
 
 if [ $currentUser == "root" ]; then
-	
-
 	# CONSTANTS
 	IMAGE_PATH="/Users/lander/ImagesFlash" #Change for your path
 	DEVICE="/dev/disk2" #Change for your device
 	FLASH_DEVICE="/dev/rdisk2" #Change for your device
 
+	# Necessary variables
 	cont=1
 	indexArray=0
 	imageArray=( )
 
+	# Set vector with images
 	for i in `ls $IMAGE_PATH`; do
 		echo $cont"- "$i
 		imageArray[$indexArray]=$i
@@ -29,27 +45,61 @@ if [ $currentUser == "root" ]; then
 		cont=$(($cont+1))
 	done
 
+	# Read selection
 	read -p "Choose the image: " flash
 
 	if [ $flash -gt ${#imageArray[@]} ] || [ $flash -lt 0 ]; then
 		clear
+		redColor
 		echo "Invalid number, launch the script again and choose one number of the list"
+		resetColor
 
 	else
 		clear
 		flash=$(($flash-1))
-		if [ "`diskutil unmountDisk $DEVICE 2>/dev/null`" ]; then
-			echo "Unmounted correctly"
-			echo "Flashing, please wait..."
-			echo ${imageArray[$flash]}
-			gunzip -c $IMAGE_PATH/${imageArray[$flash]} | pv | dd of=$FLASH_DEVICE bs=8m
+		# Check file extension 
+		EXTENSION=`echo $$IMAGE_PATH/${imageArray[$flash]} | cut -d "." -f3`
+		if [ -z $EXTENSION ]; then
+			EXTENSION=`echo $$IMAGE_PATH/${imageArray[$flash]} | cut -d "." -f2`
+			case $EXTENSION in
+				"img" ) if [ "`diskutil unmountDisk $DEVICE 2>/dev/null`" ]; then
+							echo "Unmounted correctly"
+							echo "Flashing, please wait..."
+							dd if=$IMAGE_PATH/${imageArray[$flash]} | pv | dd of=$FLASH_DEVICE bs=8m
+						else
+							clear
+							redColor
+							echo "Please, change the device number"
+							resetColor
+						fi
+					;;
+				* ) clear
+					redColor
+					echo "Unknown file extension..."
+					resetColor
+				  ;;
+			esac
 		else
-			clear
-			echo "Please, change the device number"
+			if [ "$EXTENSION" == "gz" ]; then
+				if [ "`diskutil unmountDisk $DEVICE 2>/dev/null`" ]; then
+					echo "Unmounted correctly"
+					echo "Flashing, please wait..."
+					echo ${imageArray[$flash]}
+					gunzip -c $IMAGE_PATH/${imageArray[$flash]} | pv | dd of=$FLASH_DEVICE bs=8m
+				else
+					clear
+					redColor
+					echo "Please, change the device number"
+					resetColor
+				fi
+			fi
 		fi
 	fi
 	exit 0
 else
+	clear
+	redColor
 	echo "\"sudo\" is needed..."
+	resetColor
 	exit 1
 fi
