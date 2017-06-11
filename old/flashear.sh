b8c8531bae0ca628ea4eb1a2cc7bb94b050a643a
LanderU <lander.usategui@gmail.com>
2017-06-11 20:08:29 +0200

    Update repo

---
+++ old/flashear.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+: '
+
+ maintainer Lander Usategui, e-mail: lander.usategui@gmail.com
+
+'
+
+
+clear
+
+# CONSTANTS
+IMAGE_PATH="/Users/lander/ImagesFlash" #Change for your path
+DEVICE="/dev/disk2" #Change for your device
+FLASH_DEVICE="/dev/rdisk2" #Change for your device
+
+cont=1
+indexArray=0
+imageArray=( )
+
+for i in `ls $IMAGE_PATH`; do
+	echo $cont"- "$i
+	imageArray[$indexArray]=$i
+	indexArray=$(($indexArray+1))
+	cont=$(($cont+1))
+done
+
+read -p "Choose the image: " flash
+
+if [ $flash -gt ${#imageArray[@]} ] || [ $flash -lt 0 ]; then
+	clear
+	echo "Invalid number, launch the script again and choose one number of the list"
+
+else
+	clear
+	flash=$(($flash-1))
+	if [ "`diskutil unmountDisk $DEVICE 2>/dev/null`" ]; then
+		echo "Unmounted correctly"
+		echo "Flashing, please wait..."
+		gunzip -c $IMAGE_PATH/${imageArray[$flash]} | sudo dd of=$FLASH_DEVICE bs=8m
+	else
+		clear
+		echo "Please, change the device number"
+	fi
+fi
+exit 0
