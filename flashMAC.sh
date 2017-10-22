6b05b15c8e998ea338d17d3b78a53af945600631
LanderU <lander.usategui@gmail.com>
2017-10-22 10:11:40 +0200

    flashMAC.sh: Fix style

--- flashMAC.sh
+++ flashMAC.sh
@@ -67,53 +67,53 @@ function listDevices()
 
 function checkPV()
 {
-	EXISTSPV=`command -v pv`
-	if [ -z $EXISTSPV ]; then
-		EXISTSPV="false"
-	else
-		EXISTSPV="true"
-	fi
-	echo $EXISTSPV
+  EXISTSPV=`command -v pv`
+  if [ -z $EXISTSPV ]; then
+    EXISTSPV="false"
+  else
+    EXISTSPV="true"
+  fi
+  echo $EXISTSPV
 }
 
 function checkUser()
 {
-	# Clear screen
-	clear
-	if [ $currentUser == "root" ]; then
-		listDevices
-	else
-		# Bad user
-		redColor
-		echo "\"sudo\" is needed..."
-		resetColor
-		exit 1
-	fi
+  # Clear screen
+  clear
+  if [ $currentUser == "root" ]; then
+    listDevices
+  else
+    # Bad user
+    redColor
+    echo "\"sudo\" is needed..."
+    resetColor
+    exit 1
+  fi
 }
 
 function checkImage()
 {
-	# Set vector with images
-	for i in `ls $IMAGE_PATH`; do
-		echo $cont"- "$i
-		imageArray[$indexArray]=$i
-		indexArray=$(($indexArray+1))
-		cont=$(($cont+1))
-	done
+  # Set vector with images
+  for i in `ls $IMAGE_PATH`; do
+    echo $cont"- "$i
+    imageArray[$indexArray]=$i
+    indexArray=$(($indexArray+1))
+    cont=$(($cont+1))
+  done
 
-	# Read selection
-	read -p "Choose the image: " flash
+  # Read selection
+  read -p "Choose the image: " flash
 
-	if [ $flash -gt ${#imageArray[@]} ] || [ $flash -lt 0 ]; then
-		# Bad number
-		clear
-		redColor
-		echo "Invalid number, launch the script again and choose a number of the list."
-		resetColor
-		exit 1
-	else
-		flashSd
-	fi
+  if [ $flash -gt ${#imageArray[@]} ] || [ $flash -lt 0 ]; then
+    # Bad number
+    clear
+    redColor
+    echo "Invalid number, launch the script again and choose a number of the list."
+    resetColor
+    exit 1
+  else
+    flashSd
+  fi
 }
 
 function checkSD()
@@ -130,25 +130,25 @@ function checkSD()
 
 function flashSd()
 {
-	# Clear screen
-	clear
-	flash=$(($flash-1))
-	# Check file extension
-	EXTENSION=`echo $IMAGE_PATH/${imageArray[$flash]} | cut -d "." -f3`
-	# Empty?
-	if [ -z $EXTENSION ]; then
-		# Check again
-		EXTENSION=`echo $IMAGE_PATH/${imageArray[$flash]} | cut -d "." -f2`
-		case $EXTENSION in
-			"img") # Check PV
-				   if [ $( checkPV ) == "true" ]; then
-				   		# Flash with progress bar
+  # Clear screen
+  clear
+  flash=$(($flash-1))
+  # Check file extension
+  EXTENSION=`echo $IMAGE_PATH/${imageArray[$flash]} | cut -d "." -f3`
+  # Empty?
+  if [ -z $EXTENSION ]; then
+    # Check again
+    EXTENSION=`echo $IMAGE_PATH/${imageArray[$flash]} | cut -d "." -f2`
+    case $EXTENSION in
+      "img") # Check PV
+           if [ $( checkPV ) == "true" ]; then
+              # Flash with progress bar
               checkSD
-						if [ "`diskutil unmountDisk $UNMOUNT_DEVICE 2>/dev/null`" ]; then
-							echo "Unmounted correctly"
-							echo "Flashing, please wait..."
-							SIZE=`du -h $IMAGE_PATH/${imageArray[$flash]} | cut -d "," -f1`G
-							dd if=$IMAGE_PATH/${imageArray[$flash]} | pv -s $SIZE | dd of=$FLASH_DEVICE bs=8m 2>/dev/null
+            if [ "`diskutil unmountDisk $UNMOUNT_DEVICE 2>/dev/null`" ]; then
+              echo "Unmounted correctly"
+              echo "Flashing, please wait..."
+              SIZE=`du -h $IMAGE_PATH/${imageArray[$flash]} | cut -d "," -f1`G
+              dd if=$IMAGE_PATH/${imageArray[$flash]} | pv -s $SIZE | dd of=$FLASH_DEVICE bs=8m 2>/dev/null
                if [ "`diskutil list | grep $UNMOUNT_DEVICE`" ]; then
                  `diskutil unmountDisk $UNMOUNT_DEVICE 2>/dev/null`
                  unmountCheck="`df -h | grep $UNMOUNT_DEVICE`"
@@ -164,14 +164,14 @@ function flashSd()
                    exit 1
                  fi
                fi
-						fi
-				   else
+            fi
+           else
               checkSD
-				   		if [ "`diskutil unmountDisk $UNMOUNT_DEVICE 2>/dev/null`" ]; then
-							# Flash without progress bar
-							echo "Unmounted correctly"
-							echo "Flashing, please wait..."
-							dd if=$IMAGE_PATH/${imageArray[$flash]} | dd of=$FLASH_DEVICE bs=8m 2>/dev/null
+              if [ "`diskutil unmountDisk $UNMOUNT_DEVICE 2>/dev/null`" ]; then
+              # Flash without progress bar
+              echo "Unmounted correctly"
+              echo "Flashing, please wait..."
+              dd if=$IMAGE_PATH/${imageArray[$flash]} | dd of=$FLASH_DEVICE bs=8m 2>/dev/null
               if [ "`diskutil list | grep $UNMOUNT_DEVICE`" ]; then
                 diskutil unmountDisk $UNMOUNT_DEVICE 2>/dev/null
                 unmountCheck="`df -h | grep $UNMOUNT_DEVICE`"
@@ -189,84 +189,84 @@ function flashSd()
               fi
             fi
           fi
-			;;
-			* ) clear
-				redColor
-				echo "Unknown file extension..."
-				resetColor
-				exit 1
-			;;
-		esac
-	else
-		case $EXTENSION in
-			"gz") 	# Check PV
-				   if [ $( checkPV ) == "true" ]; then
-				   		# Flash with progress bar
-            					checkSD
-						if [ "`diskutil unmountDisk $UNMOUNT_DEVICE 2>/dev/null`" ]; then
-							echo "Unmounted correctly"
-							echo "Flashing, please wait..."
-							SIZE=`du -h $IMAGE_PATH/${imageArray[$flash]} | cut -d "," -f1`G
-							gunzip -c $IMAGE_PATH/${imageArray[$flash]} | pv -s 7g | dd of=$FLASH_DEVICE bs=8m 2>/dev/null
-              if [ "`diskutil list | grep $UNMOUNT_DEVICE`" ]; then
-                diskutil unmountDisk $UNMOUNT_DEVICE 2>/dev/null
-                unmountCheck="`df -h | grep $UNMOUNT_DEVICE`"
-                if [ -z "${unmountCheck}" ]; then
-                  clear
-                  echo -e "Done\nRemove your SD Card"
-                  exit 0
-                else
-                  clear
-                  redColor
-                  echo "Unable to unmount the SD Card"
-                  resetColor
-                  exit 1
-                fi
-              fi
-						fi
-				   else
-              					checkSD
-				   		if [ "`diskutil unmountDisk $UNMOUNT_DEVICE 2>/dev/null`" ]; then
-							# Flash without progress bar
-							echo "Unmounted correctly"
-							echo "Flashing, please wait..."
-							gunzip -c $IMAGE_PATH/${imageArray[$flash]} | dd of=$FLASH_DEVICE bs=8m 2>/dev/null
-              if [ "`diskutil list | grep $UNMOUNT_DEVICE`" ]; then
-                diskutil unmountDisk $UNMOUNT_DEVICE 2>/dev/null
-                unmountCheck="`df -h | grep $UNMOUNT_DEVICE`"
-                if [ -z "${unmountCheck}" ]; then
-                  clear
-                  echo -e "Done\nRemove your SD Card"
-                  exit 0
-                else
-                  clear
-                  redColor
-                  echo "Unable to unmount the SD Card"
-                  resetColor
-                  exit 1
-                fi
-              fi
-						else
-							clear
-							redColor
-							echo "Please, change the device number"
-							resetColor
-							exit 1
-						fi
-				   fi
       ;;
-			"img")
-					EXTENSION=`echo $IMAGE_PATH/${imageArray[$flash]} | cut -d "." -f4`
-					if [ -z $EXTENSION ]; then
-					   # Check PV
+      * ) clear
+        redColor
+        echo "Unknown file extension..."
+        resetColor
+        exit 1
+      ;;
+    esac
+  else
+    case $EXTENSION in
+      "gz")   # Check PV
+           if [ $( checkPV ) == "true" ]; then
+              # Flash with progress bar
               checkSD
-					    if [ $( checkPV ) == "true" ]; then
-					   		# Flash with progress bar
-							if [ "`diskutil unmountDisk $UNMOUNT_DEVICE 2>/dev/null`" ]; then
-								echo "Unmounted correctly"
-								echo "Flashing, please wait..."
-								SIZE=`du -h $IMAGE_PATH/${imageArray[$flash]} | cut -d "," -f1`G
-								dd if=$IMAGE_PATH/${imageArray[$flash]} | pv -s $SIZE | dd of=$FLASH_DEVICE bs=8m 2>/dev/null
+            if [ "`diskutil unmountDisk $UNMOUNT_DEVICE 2>/dev/null`" ]; then
+              echo "Unmounted correctly"
+              echo "Flashing, please wait..."
+              SIZE=`du -h $IMAGE_PATH/${imageArray[$flash]} | cut -d "," -f1`G
+              gunzip -c $IMAGE_PATH/${imageArray[$flash]} | pv -s 7g | dd of=$FLASH_DEVICE bs=8m 2>/dev/null
+              if [ "`diskutil list | grep $UNMOUNT_DEVICE`" ]; then
+                diskutil unmountDisk $UNMOUNT_DEVICE 2>/dev/null
+                unmountCheck="`df -h | grep $UNMOUNT_DEVICE`"
+                if [ -z "${unmountCheck}" ]; then
+                  clear
+                  echo -e "Done\nRemove your SD Card"
+                  exit 0
+                else
+                  clear
+                  redColor
+                  echo "Unable to unmount the SD Card"
+                  resetColor
+                  exit 1
+                fi
+              fi
+            fi
+           else
+              checkSD
+              if [ "`diskutil unmountDisk $UNMOUNT_DEVICE 2>/dev/null`" ]; then
+              # Flash without progress bar
+              echo "Unmounted correctly"
+              echo "Flashing, please wait..."
+              gunzip -c $IMAGE_PATH/${imageArray[$flash]} | dd of=$FLASH_DEVICE bs=8m 2>/dev/null
+              if [ "`diskutil list | grep $UNMOUNT_DEVICE`" ]; then
+                diskutil unmountDisk $UNMOUNT_DEVICE 2>/dev/null
+                unmountCheck="`df -h | grep $UNMOUNT_DEVICE`"
+                if [ -z "${unmountCheck}" ]; then
+                  clear
+                  echo -e "Done\nRemove your SD Card"
+                  exit 0
+                else
+                  clear
+                  redColor
+                  echo "Unable to unmount the SD Card"
+                  resetColor
+                  exit 1
+                fi
+              fi
+            else
+              clear
+              redColor
+              echo "Please, change the device number"
+              resetColor
+              exit 1
+            fi
+           fi
+      ;;
+      "img")
+          EXTENSION=`echo $IMAGE_PATH/${imageArray[$flash]} | cut -d "." -f4`
+          if [ -z $EXTENSION ]; then
+             # Check PV
+              checkSD
+              if [ $( checkPV ) == "true" ]; then
+                # Flash with progress bar
+              if [ "`diskutil unmountDisk $UNMOUNT_DEVICE 2>/dev/null`" ]; then
+                echo "Unmounted correctly"
+                echo "Flashing, please wait..."
+                SIZE=`du -h $IMAGE_PATH/${imageArray[$flash]} | cut -d "," -f1`G
+                dd if=$IMAGE_PATH/${imageArray[$flash]} | pv -s $SIZE | dd of=$FLASH_DEVICE bs=8m 2>/dev/null
                 if [ "`diskutil list | grep $UNMOUNT_DEVICE`" ]; then
                   diskutil unmountDisk $UNMOUNT_DEVICE 2>/dev/null
                   unmountCheck="`df -h | grep $UNMOUNT_DEVICE`"
@@ -282,15 +282,15 @@ function flashSd()
                     exit 1
                   fi
                 fi
-							fi
-					    else
+              fi
+              else
                 #check SD
                 checkSD
-					   		if [ "`diskutil unmountDisk $UNMOUNT_DEVICE 2>/dev/null`" ]; then
-  								# Flash without progress bar
-  								echo "Unmounted correctly"
-  								echo "Flashing, please wait..."
-  								dd if=$IMAGE_PATH/${imageArray[$flash]} | dd of=$FLASH_DEVICE bs=8m
+                if [ "`diskutil unmountDisk $UNMOUNT_DEVICE 2>/dev/null`" ]; then
+                  # Flash without progress bar
+                  echo "Unmounted correctly"
+                  echo "Flashing, please wait..."
+                  dd if=$IMAGE_PATH/${imageArray[$flash]} | dd of=$FLASH_DEVICE bs=8m
                   if [ "`diskutil list | grep $UNMOUNT_DEVICE`" ]; then
                     diskutil unmountDisk $UNMOUNT_DEVICE 2>/dev/null
                     unmountCheck="`df -h | grep $UNMOUNT_DEVICE`"
@@ -307,39 +307,39 @@ function flashSd()
                     fi
                   fi
                 fi
-					    fi
-					else
+              fi
+          else
             redColor
-						echo "zip version?? extract image first..."
+            echo "zip version?? extract image first..."
             resetColor
-						exit 1
-					fi
-			;;
-			* ) clear
-				redColor
-				echo "Unknown file extension..."
-				resetColor
-				exit 1
-			;;
-		esac
-	fi
+            exit 1
+          fi
+      ;;
+      * ) clear
+        redColor
+        echo "Unknown file extension..."
+        resetColor
+        exit 1
+      ;;
+    esac
+  fi
 }
 
 # COLORS
 
 function redColor()
 {
-	tput setaf 1
+  tput setaf 1
 } # End red color
 
 function resetColor()
 {
-	tput sgr 0
+  tput sgr 0
 } # End Reset color
 
 function main()
 {
-	checkUser
+  checkUser
 }
 
 # Start script
