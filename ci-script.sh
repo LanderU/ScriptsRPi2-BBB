40fd5feda92a4c60f50cc6abd9969b18280a285f
Lander Usategui San Juan <lander.usategui@gmail.com>
2019-09-12 12:12:35 +0200

    Create ci-script.sh
    Signed-off-by: LanderU lander.usategui@gmail.com

---
+++ ci-script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+files_to_check=$(find . -type f -print0 | xargs --null grep -c "/bin/bash" | grep -v ":0" | grep -v "old" | cut -d ":" -f1)
+files_invalid=( )
+count=0;
+for i in ${files_to_check}; do
+	shellcheck "${i}" > /dev/null
+	result=$?
+	if [ ${result} -ne 0 ]; then
+		files_invalid[${count}]="${i}"
+		count=$((count+1))
+	fi
+done
+
+if [ ${#files_invalid[@]} -ne 0 ]; then
+
+	echo "Errores"
+else
+	echo "No errores"
+fi
